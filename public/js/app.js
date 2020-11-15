44816c30c04fb898289e9ca8e6470b6086d83b83
stormwild <artorrijos@gmail.com>
2020-11-15 14:17:13 +0000

    Chapter 2 complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/serviceworker.js")
+    .then(function (registration) {
+      console.log(
+        "Service Worker registered with scope",
+        registration.scope,
+        new Date().toLocaleTimeString()
+      );
+    })
+    .catch(function (err) {
+      console.log("Service Worker registration failed", err);
+    });
+}
+
+$(document).ready(function () {
   // Fetch and render upcoming events in the hotel
   $.getJSON("/events.json", renderEvents);
 });
-
-
-
 
 /* ************************************************************ */
 /* The code below this point is used to render to the DOM. It   */
@@ -14,15 +26,25 @@ $(document).ready(function() {
 /* @TalAter                                                     */
 /* ************************************************************ */
 
-var renderEvents = function(data) {
-  data.forEach(function(event) {
+var renderEvents = function (data) {
+  data.forEach(function (event) {
     $(
-      "<div class=\"col-lg-2 col-md-4 col-sm-6 event-container\"><div class=\"event-card\">"+
-      "<div class=\"event-date\">"+event.date+"</div>"+
-      "<img src=\""+event.img+"\" alt=\""+event.title+"\" class=\"img-responsive\" />"+
-      "<h4>"+event.title+"</h4>"+
-      "<p>"+event.description+"</p>"+
-      "</div></div>"
+      '<div class="col-lg-2 col-md-4 col-sm-6 event-container"><div class="event-card">' +
+        '<div class="event-date">' +
+        event.date +
+        "</div>" +
+        '<img src="' +
+        event.img +
+        '" alt="' +
+        event.title +
+        '" class="img-responsive" />' +
+        "<h4>" +
+        event.title +
+        "</h4>" +
+        "<p>" +
+        event.description +
+        "</p>" +
+        "</div></div>"
     ).insertBefore("#events-container div.calendar-link-container");
   });
 };
